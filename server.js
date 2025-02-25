12d573a1d684b6b8fe79a812894d4346343553e8
Koop Reynders <k.g.reynders@hva.nl>
2025-02-25 12:13:06 +0100

    workshop user generated content code

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 import express from 'express'
-
 import { Liquid } from 'liquidjs';
 
-
 // Vul hier jullie team naam in
-const teamName = '';
-
+const teamName = 'Rocket';
 
 const app = express()
-
 app.use(express.static('public'))
-
 const engine = new Liquid();
 app.engine('liquid', engine.express()); 
-
 app.set('views', './views')
-
 app.use(express.urlencoded({extended: true}))
+
+
+
+// MESSAGES
+let messages = []
+// messages.push("hoi ik ben een bericht","het tweede bericht","het derde bericht")
+app.get('/berichten', async function (request, response) {
+  response.render('messages.liquid', {
+    // persons: personResponseJSON.data, 
+    messages: messages
+  })
+})
+app.post('/berichten', async function (request, response) {
+//data verwerken
+  messages.push(request.body.tekstje)
+  response.redirect(303, '/berichten')
+})
+
+
 
 
 app.get('/', async function (request, response) {
@@ -46,12 +58,16 @@ app.post('/', async function (request, response) {
 })
 
 
-app.set('port', process.env.PORT || 8000)
 
-if (teamName == '') {
-  console.log('Voeg eerst de naam van jullie team in de code toe.')
-} else {
-  app.listen(app.get('port'), function () {
-    console.log(`Application started on http://localhost:${app.get('port')}`)
-  })
-}
+app.set('port', process.env.PORT || 8000)
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+// if (teamName == '') {
+//   console.log('Voeg eerst de naam van jullie team in de code toe.')
+// } else {
+//   app.listen(app.get('port'), function () {
+//     console.log(`Application started on http://localhost:${app.get('port')}`)
+//   })
+// }
